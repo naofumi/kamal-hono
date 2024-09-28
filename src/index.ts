db6958efa3594891ac425130262c2ccdfb68c663
Naofumi Kagami <naofumi@mac.com>
2024-09-28 10:11:30 +0900

    Create some pages

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { serveStatic } from '@hono/node-server/serve-static'
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.use('/static/*', serveStatic({ root: './' }))
 
 const port = 3000
 console.log(`Server is running on port ${port}`)
