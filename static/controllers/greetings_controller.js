c25ae49cde51b79108477123d16a3c153ac86a01
Naofumi Kagami <naofumi@mac.com>
2024-12-18 10:53:38 +0900

    Greetings controller

--- static/controllers/greetings_controller.js
+++ static/controllers/greetings_controller.js
@@ -1,8 +1,8 @@
 import {Controller} from "stimulus"
 
-// Connects to data-controller="accordion"
+// Connects to data-controller="greetings"
 export default class extends Controller {
   connect() {
-    alert("Howdy!")
+    this.element.innerHTML = "<h1 class='text-3xl'>Hello from Stimulus Greetings Controller</h1>"
   }
 }
